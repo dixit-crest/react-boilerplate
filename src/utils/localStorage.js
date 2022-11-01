b507ac087c47dc3ff47ba3956f40f276c0de5956
dixit-crest <dixit.p@crestinfosystems.net>
2022-11-01 15:50:53 +0530

    Adding example .env

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -3,19 +3,21 @@ import { LOCAL_STORAGE_USER } from "./constants";
 export const LocalUtils = {
   /**
    *
-   * @param {}
-   * @return {String|null} TOKEN
+   * @param {String} newToken - newToken
+   * Set the token in localstorage
    */
-  getToken: () => {
+  setToken: (newToken) => {
     const user = JSON.parse(localStorage.getItem(LOCAL_STORAGE_USER));
-    if (!user) return null;
-    return user.token;
+    if (!user || !newToken) return null;
+    user.token = newToken
+    localStorage.setItem(LOCAL_STORAGE_USER, JSON.stringify(user))    
   },
  
   /**
    *
    * @param {}
    * @return {Object|null} User object
+   * Returns the token from localstorage
    */
   getToken: () => {
     const user = JSON.parse(localStorage.getItem(LOCAL_STORAGE_USER));
